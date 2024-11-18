a99848ba3bfd7c011a2022c9ef654dc19785cd6f
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-11-18 09:04:09 -0500

    Added cross origins

--- src/main/java/com/example/cooking_lab_personal_recipe_api/api/controller/RecipeController.java
+++ src/main/java/com/example/cooking_lab_personal_recipe_api/api/controller/RecipeController.java
@@ -3,6 +3,7 @@ package com.example.cooking_lab_personal_recipe_api.api.controller;
 import com.example.cooking_lab_personal_recipe_api.api.model.Recipe;
 import com.example.cooking_lab_personal_recipe_api.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "https://cooking-lab-personal-recipe-api.onrender.com"})
     @GetMapping("/api/recipes/personal")
     public List<Recipe> getRecipe(@RequestParam(required = false) String owner, @RequestParam(required = false) Integer id) {
         if (id != null && owner != null) {
